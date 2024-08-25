1052dcd06d4b119bcbe0cda406a6db4a5bc6abf9
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2024-08-25 18:55:57 +0530

    Day-01 🚀

--- 30 Days of code/Day-01.py
+++ 30 Days of code/Day-01.py
@@ -1,4 +1,4 @@
-# Task - Print Hello, World . on the first line, and the contents of inputString on the second line.
+# Task - Print 'Hello, World.' on the first line, and the contents of input_string on the second line.
 
 input_string = input()
 print('Hello World!')
