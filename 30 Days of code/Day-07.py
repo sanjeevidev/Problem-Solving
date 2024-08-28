ba40932472c0df22e1181c6f3fbd14e4efcb49f4
Sanjeevi Kumar V <96916376+sanjeevidev@users.noreply.github.com>
2024-08-28 23:27:13 +0530

    Day-07 🚀

---
+++ 30 Days of code/Day-07.py
@@ -0,0 +1,7 @@
+# Task - Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+n = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+for i in range(len(arr)-1, -1, -1):
+  print(arr[i], end=' ')
